62fcd5b04bed6d7286a602f6eea9695cfff4dc4f
Tim Riley <tim@riley.id.au>
2024-12-03 21:51:43 +1100

    Render guide roots from index.md files (#2)

--- app/actions/guides/show.rb
+++ app/actions/guides/show.rb
@@ -5,6 +5,15 @@ module Site
     module Guides
       class Show < Site::Action
         def handle(request, response)
+          # Return 404 when "index" path is given explicitly, since we use this for the guide root.
+          halt 404 if request.params[:path] == "index"
+
+          # When no path is given, we're at the guide's root. Now we can set the path to "index" in
+          # order to render the index page.
+          params = request.params.to_h
+          params[:path] ||= "index"
+
+          response.render(view, **params)
         end
       end
     end
