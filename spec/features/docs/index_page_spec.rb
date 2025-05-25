345ed73580cb4fdcbecca255b6ea4c55e3cf0733
Josephine Hall <josephinehall@gmail.com>
2025-05-25 17:06:17 +1000

    Fix test

--- spec/features/docs/index_page_spec.rb
+++ spec/features/docs/index_page_spec.rb
@@ -7,8 +7,8 @@ RSpec.feature "Docs / Index page" do
     within "[data-testid=dry-rb-docs]" do
       expect(page.find_all("li a").map(&:text)[0..2]).to eq [
         "dry-auto_inject",
-        "dry-cli",
-        "dry-operation"
+        "dry-equalizer",
+        "dry-cli"
       ]
     end
   end
