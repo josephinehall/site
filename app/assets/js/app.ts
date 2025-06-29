da388f2ac81c474a4a93a846ba4e99a69a337e3c
Max Wheeler <max@makenosound.com>
2025-06-29 12:08:22 +1000

    Add TypeScript config (#62)
    This PR adds a basic TypeScript configuration.
    
    Changes:
    
    - Add `typescript` to `devDependencies`
    - Add `tsconfig.json`
    - Convert existing code to TS
    - Add linting step in CI

--- app/assets/js/app.ts
+++ app/assets/js/app.ts
@@ -1,7 +1,19 @@
-import "../css/app.css";
+import "~css/app.css";
 import "@docsearch/css";
 
 import docsearch from "@docsearch/js";
+
+interface AppSettings {
+  docsearch_app_id: string;
+  docsearch_api_key: string;
+  docsearch_index_name: string;
+}
+
+declare global {
+  interface Window {
+    app_settings: AppSettings;
+  }
+}
 
 docsearch({
   container: "#docsearch",
